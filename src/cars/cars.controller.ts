023502e885c926db9ba028c81a345cfdf286e713
vhtoledo <querynetworking@gmail.com>
2023-08-30 12:31:40 -0300

    creacion de cars controllers

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('cars')
+export class CarsController {
+
+    @Get()
+    getAllCars() {
+        return ['Toyota','Honda','Jeep']
+    }
+}
