41fbab2e8535ae22c7a71e5b4a8f6232bedcdc21
vhtoledo <querynetworking@gmail.com>
2023-09-01 09:54:55 -0300

    creacion de post, patch y delete en controllers

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -18,5 +18,26 @@ export class CarsController {
       console.log( {id} );
       return this.carsService.findOneById( id );
     }
+
+    @Post()
+    createCart( @Body() body: any) {
+      return body;
+    }
+
+    @Patch(':id')
+    updateCart( 
+      @Param('id', ParseIntPipe) id: number,
+      @Body() body: any) {
+      return body;
+    }
+
+    @Delete(':id')
+    deleteCart( @Param('id', ParseIntPipe) id: number ) {
+      return {
+        method: 'delete',
+        id
+      };
+    }
+  
   
 }
