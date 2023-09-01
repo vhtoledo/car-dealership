3eeb7493739b33c62c85dbdd0af548530ee1279a
vhtoledo <querynetworking@gmail.com>
2023-09-01 11:57:07 -0300

    eliminar carro

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -71,5 +71,10 @@ export class CarsService {
         return carDB;
     }
 
+    delete( id: string ) {
+        const car = this.findOneById( id );
+        this.cars = this.cars.filter( car => car.id !== id );
+    }
+
 
 }
