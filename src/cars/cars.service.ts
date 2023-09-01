829bcb0106984aada7e44f69cabbfd1ad0eb0015
vhtoledo <querynetworking@gmail.com>
2023-09-01 11:50:02 -0300

    crear nuevo carro

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { v4 as uuid } from 'uuid';
+import { CreateCarDto } from './dto/create-car.dto';
+import { Car } from './interfaces/car.interfaces';
 
 @Injectable()
 export class CarsService {
@@ -32,6 +34,18 @@ export class CarsService {
 
         if ( !car ) throw new NotFoundException(`Car with id '${id}' not found`);
         
+
+        return car;
+    }
+
+    create( createCarDto: CreateCarDto ) {
+
+        const car: Car = {
+            id: uuid(),
+            ...createCarDto
+        }
+
+        this.cars.push( car );
 
         return car;
     }
