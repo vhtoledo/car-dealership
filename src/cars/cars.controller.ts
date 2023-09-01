def6a94ceca113ee62ff2152a1ba9d8aea593ce8
vhtoledo <querynetworking@gmail.com>
2023-09-01 12:07:30 -0300

    actualizacion de controllers para metodos patch y delete

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { CreateCarDto } from './dto/create-car.dto';
+import { UpdateCarDto } from './dto';
 
 @Controller('cars')
 export class CarsController {
@@ -26,18 +27,16 @@ export class CarsController {
     }
 
     @Patch(':id')
-    updateCart( 
-      @Param('id', ParseIntPipe) id: number,
-      @Body() body: any) {
-      return body;
+    updateCar( 
+      @Param('id', ParseUUIDPipe ) id: string, 
+      @Body() updateCarDto: UpdateCarDto ) 
+    {
+      return this.carsService.update( id, updateCarDto );
     }
 
     @Delete(':id')
-    deleteCart( @Param('id', ParseIntPipe) id: number ) {
-      return {
-        method: 'delete',
-        id
-      };
+    deleteCar( @Param('id', ParseUUIDPipe ) id: string ) {
+      return this.carsService.delete( id )
     }
   
   
