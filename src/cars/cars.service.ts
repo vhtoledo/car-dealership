935cc9571553d9054b5cdab21c41dc8542484294
vhtoledo <querynetworking@gmail.com>
2023-09-04 08:48:19 -0300

    creacion de seed para cargar datos, preparacion de servicios e inyectar servicios.

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -8,21 +8,11 @@ import { UpdateCarDto } from './dto';
 export class CarsService {
 
     private cars = [
-        {
-            id: uuid(),
-            brand: 'Toyota',
-            model: 'Corolla' 
-        },
-        {
-            id: uuid(),
-            brand: 'Honda',
-            model: 'Civic' 
-        },
-        {
-            id: uuid(),
-            brand: 'Jeep',
-            model: 'Cherokee' 
-        },
+        //{
+        //    id: uuid(),
+        //    brand: 'Toyota',
+        //    model: 'Corolla' 
+        //},
     ];
 
     findAll() {
@@ -74,6 +64,10 @@ export class CarsService {
     delete( id: string ) {
         const car = this.findOneById( id );
         this.cars = this.cars.filter( car => car.id !== id );
+    }
+
+    fillCarsWithSeedData( cars: Car[] ) {
+        this.cars = cars;
     }
 
 
---
+++ src/seed/data/cars.seed.ts
@@ -0,0 +1,21 @@
+import { v4 as uuid } from 'uuid';
+import { Car } from 'src/cars/interfaces/car.interfaces';
+
+
+export const CARS_SEED: Car[] = [
+    {
+        id: uuid(),
+        brand: 'Toyota',
+        model: 'Corolla',
+    },
+    {
+        id: uuid(),
+        brand: 'Honda',
+        model: 'Civic',
+    },
+    {
+        id: uuid(),
+        brand: 'Jeep',
+        model: 'Cherokee',
+    },
+]
